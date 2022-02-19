89480aa3f413d76158f810ef049db49ac7cea885
simransachdevaaa <99955528+simransachdevaaa@users.noreply.github.com>
2022-02-19 13:41:08 +0530

    Delete NeedOrLend/Vote-Right-ACM-Hacks-6.0-main directory

--- NeedOrLend/Vote-Right-ACM-Hacks-6.0-main/assets/js/verification.js
+++
@@ -1,9 +0,0 @@
-function onScanSuccess(qrCodeMessage) {
-    document.getElementById('result').innerHTML = '<span class="result">'+qrCodeMessage+'</span>';
-}
-function onScanError(errorMessage) {
-  //handle scan error
-}
-var html5QrcodeScanner = new Html5QrcodeScanner(
-    "reader", { fps: 10, qrbox: 250 });
-html5QrcodeScanner.render(onScanSuccess, onScanError);
